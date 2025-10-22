0fcff202867952ec552f0c99444572d5879864a8
wyclif001 <wyclifgachara@gmail.com>
2025-10-22 21:08:52 +0300

    setting up the env

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+
+import express from 'express'
+import dotenv from 'dotenv';
+import { getPool } from './db/config';
+//import route later   
+
+const app = express()
+
+dotenv.config();
+//middleware
+app.use(express.json()); 
+
+const port = process.env.PORT || 8081;
+
+//register routes ie,
+
+//Root route
+app.get('/', (req, res) => {
+    res.send("Hello, express API is running...");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: http://localhost:${port}`);
+})
+
+//test database connection
+getPool()
+    .then(() => console.log("Database connected"))
+    .catch((err: any) => console.log("Database connection failed: ", err));
+
