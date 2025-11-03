b96294947bbdb095bb3207e5c57e42ed5406c023
Mutun3 <mutunesamuel5@gmail.com>
2025-11-03 20:33:58 +0300

    Leave Balance and Notifications Table Added

---
+++ src/types/notification.types.ts
@@ -0,0 +1,20 @@
+export interface Notification{
+    notification_id:number;
+    employee_id: number;
+    request_id:number;
+    message:string;
+}
+
+export interface NewNotification{
+    notification_id:number;
+    employee_id: number;
+    request_id:number;
+    message:string;
+}
+
+export interface UpdateNotification{
+    notification_id?:number;
+    employee_id?: number;
+    request_id?:number;
+    message?:string;
+}
